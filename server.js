172e7ac3ceb9a80292658eb40ad5dfb15a67905f
cym2050 <1390062269@qq.com>
2020-07-29 22:20:56 +0800

    change the /

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get('/', (req, res) => { res.send(database.users) })
+app.get('/', (req, res) => { res.send('it is working')})
 app.post('/signin', (req, res) => { signin.handleSignin(req, res, knex, bcrypt)})
 app.post('/register', (req, res) => { register.handleRegister(req, res, knex, bcrypt)})
 app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, knex)})
