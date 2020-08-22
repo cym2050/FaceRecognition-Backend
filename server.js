5bfa2b0b9a7cabbc1516dff0c97546be059a66b5
cym2050 <1390062269@qq.com>
2020-08-23 00:23:36 +0800

    查找官方文档，fix

--- server.js
+++ server.js
@@ -6,7 +6,9 @@ const knex = require('knex')({
   client: 'pg',
   connection: {
     connectionString : process.env.DATABASE_URL,
-    ssl : true, 
+    ssl : {
+      rejectUnauthorized : false
+    }
   }
 });
 
